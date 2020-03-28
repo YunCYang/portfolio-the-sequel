22d06bdde3ce8faa21251572ccfb12fb420b96c1
YunCYang <ericeorl2013@gmail.com>
2020-03-27 19:48:03 -0700

    style adjustion

--- client/util/cursorRing.js
+++ client/util/cursorRing.js
@@ -24,7 +24,7 @@ const cursorRing = () => {
   const renderRing = () => {
     ringX = trace(ringX, cursorX, 0.2);
     ringY = trace(ringY, cursorY, 0.2);
-    ring[0].style.transform = `translate(${ringX - 18}px, ${ringY - 18}px)`;
+    ring[0].style.transform = `translate(${ringX - 21}px, ${ringY - 21}px)`;
     requestAnimationFrame(renderRing);
   };
   requestAnimationFrame(renderRing);
