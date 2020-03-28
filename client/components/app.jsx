85902e6835f11da15f92964cbd07f68784ce00aa
YunCYang <ericeorl2013@gmail.com>
2020-03-27 19:40:29 -0700

    added simple cursor ring

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import cursorRing from '../util/cursorRing';
 
 const App = () => {
 
+  cursorRing();
+
   return (
-    <></>
+    <div>
+      <div className='pointer-ring'></div>
+    </div>
   );
 };
 
