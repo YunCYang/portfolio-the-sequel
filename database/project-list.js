201a0f0e724930039aa4ad2d9040b32fcfcc4a31
YunCYang <ericeorl2013@gmail.com>
2020-04-07 23:00:43 -0700

    added title, detail, tech, header in the project section

--- database/project-list.js
+++ database/project-list.js
@@ -2,18 +2,18 @@ const projectList = [
   {
     id: 1,
     name: 'Lacerna',
-    tech: ['ReactJs', 'NodeJs', 'PostgreSQL', 'Redux'],
+    tech: ['React', 'Node', 'PostgreSQL', 'Redux'],
     github: 'https://github.com/YunCYang/lacerna',
     live: 'https://lacerna.yuncyang.com/',
     desc: 'A demo e-commerce site developed with ReactJs',
-    detail: 'Lacerna is a demo e-commerce site selling coats, capes and similar apparels. Lacerna mimics real-life online stores with home page promoting new and popular products, product page that lets you see product detail and add it to the shopping cart, and cart page allowing you to proceed the order. User can sign up as a user or shop as a guest.',
+    detail: 'Lacerna is a demo e-commerce site selling coats, capes and similar apparels. Lacerna mimics real-life online stores with home page promoting new and popular products, product page that lets you see product detail, and cart page allowing you to proceed with the order. User can sign up as a user or shop as a guest.',
     img: './images/lacerna.png',
     img2: './images/lacerna_product.png',
     img3: './images/lacerna_cart.png'
   }, {
     id: 2,
     name: 'Sudo-Ku',
-    tech: ['ReactJs', 'Jest', 'Sass'],
+    tech: ['React', 'Jest', 'Sass'],
     github: 'https://github.com/YunCYang/sudo-ku',
     live: 'https://sudo-ku.yuncyang.com/',
     desc: 'A sudoku game that recursively creates puzzle',
